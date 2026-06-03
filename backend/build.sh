98ea33cb37de714df5092a80f70f1490525ada59
akraradets <akraradets@gmail.com>
2026-06-03 13:48:38 +0700

    chore: update Docker setup and versioning script; add .env.example template

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
+
+# The script try to realize the VERSION based on 
+# 1. git tag
+# if missing git tag,
+
 export TAG=$(git tag --points-at HEAD)
-echo "tag=${TAG}"
-export VERSION=$(uv version ${TAG} --short)
+
+# if this commit has a tag
+if TAG
+
+export VERSION=$(uv version --short)
+echo "Current version: $VERSION"
+
+NEW_VERSION=$1
+
+if [[ -z "$NEW_VERSION" ]]; then
+    read -p "Do you want to change the version? (y/N): " CHANGE_VERSION
+    if [[ "$CHANGE_VERSION" =~ ^[Yy]$ ]]; then
+        read -p "Enter new version: " NEW_VERSION
+    fi
+fi
+
+if [[ -n "$NEW_VERSION" ]]; then
+    VERSION=$(uv version ${NEW_VERSION} --short)
+fi
+
+export TAG="v${VERSION}"
+echo "TAG: $TAG"
 cd dist && docker buildx bake --allow=fs.read=..
